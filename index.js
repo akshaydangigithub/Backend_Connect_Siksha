daaa398085d9fb2225c75bf0e745abd709841e67
Hariom dangi <hdangi224@gmail.com>
2024-07-31 19:29:31 +0530

    create a user with mongoDB

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./models/userModel");
 
 const app = express();
 const port = 8080;
 
-// get -> read
-// post -> create
-// put -> update
-// delete -> delete
+connectDB();
 
 app.get("/", (request, response) => {
   response.send("Hello World From express");
@@ -14,6 +13,23 @@ app.get("/", (request, response) => {
 
 app.get("/about", (request, response) => {
   response.send("About Page");
+});
+
+app.post("/create", async (request, response) => {
+  try {
+    const newUser = await new User({
+      name: "Akshay Dangi",
+      email: "Akuu@gamil.com",
+      password: "skcweifhrfheiwh",
+      phone: 7898860838,
+    });
+
+    await newUser.save();
+
+    response.json(newUser);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
